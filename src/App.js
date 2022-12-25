88ade935e3c1a2ee7d6d2b70d601f74437825672
neeleshrohit <87979871+neeleshrohit@users.noreply.github.com>
2022-12-25 17:42:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ function App() {
         <p>
           Neelesh Singh (React Project HOST Using of AWS Pipe Line + s3 Bucket +EC2)
         </p>
-        <a
-          className="App-link">
+       
           <img src="https://www.funimada.com/assets/images/cards/big/congrats-1.gif" alt="rohit"/>
-        </a>
+        
       </header>
     </div>
   );
