e013bc1a6562eea54a0ff5a6cf5d62779fb98ea3
neeleshrohit <87979871+neeleshrohit@users.noreply.github.com>
2022-12-25 17:37:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,8 @@ function App() {
           Neelesh Singh (React Project HOST Using of AWS Pipe Line + s3 Bucket +EC2)
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          className="App-link">
+          <img url:"https://www.funimada.com/assets/images/cards/big/congrats-1.gif"/>
         </a>
       </header>
     </div>
